591b16c36ce2e5cfbe46538729649e760cb9a9cc
jhon <jfinfocesde@gmail.com>
2025-03-03 23:13:13 -0500

    init

---
+++ src/main/java/com/cesde/proyecto_integrador/repository/AsistenciaRepository.java
@@ -0,0 +1,10 @@
+package com.cesde.proyecto_integrador.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cesde.proyecto_integrador.model.Asistencia;
+
+
+public interface AsistenciaRepository extends JpaRepository<Asistencia, Long> {
+    
+}
